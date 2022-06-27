7dc462e2e27e3b5cb240668a4f211163d60a19ce
Alex <alex.pisckunoff@yadnex.ru>
2022-06-27 09:21:02 +0300

    fixed first example

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-URL="./provskell.html"
-SERVER="./server.js"
-[[ -x $BROWSER ]] && exec "$BROWSER" "$URL"
-path=$(which xdg-open || which gnome-open) && exec "$path" "$URL" &
-exec node "$SERVER"
-echo "Can't find browser"
+
+add-apt-repository universe ;
+apt-get update ;
+apt-get install nodejs ;
+apt-get install cabal-install ;
+nodejs -v ; 
+cabal -V ;
